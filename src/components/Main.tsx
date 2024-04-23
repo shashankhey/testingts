b25f94c7366acdfc2a05bf3c1fd0d6559ce5a4a4
shashankhey <shashankhey1710@gmail.com>
2024-04-23 19:14:09 +0530

    first commit

---
+++ src/components/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Main = () => {
+    const adFa_23423 = 12;
+    console.log(adFa_23423);
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Main;
